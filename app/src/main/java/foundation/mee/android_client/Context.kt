467ad5be3d288c23544a2b548f6ad11ab258f2fc
Anthony Ivanov <aivanov@swiftinvention.com>
2023-03-13 21:12:24 +0100

    draft

--- app/src/main/java/foundation/mee/android_client/Context.kt
+++ app/src/main/java/foundation/mee/android_client/Context.kt
@@ -1,3 +1,22 @@
 package foundation.mee.android_client
 
-data class Context(var id: String, val did: String, var claims: List<ConsentRequestClaim>, val clientMetadata: PartnerMetadata)
+class Context {
+    val id: String
+    val did: String
+    var claims: List<ConsentRequestClaim>
+    val clientMetadata: PartnerMetadata
+
+    constructor(
+        id: String,
+        did: String,
+        claims: List<ConsentRequestClaim>,
+        clientMetadata: PartnerMetadata) {
+        this.id = id
+        this.did = did
+        this.claims = claims
+        this.clientMetadata = clientMetadata
+    }
+
+
+
+}
--- app/src/main/java/foundation/mee/android_client/MeeAgentStore.kt
+++ app/src/main/java/foundation/mee/android_client/MeeAgentStore.kt
@@ -1,10 +1,57 @@
 package foundation.mee.android_client
 
-import uniffi.mee_agent.MeeAgent
+import android.content.ContextWrapper
+import androidx.activity.ComponentActivity
+import uniffi.mee_agent.*
+
 
 class MeeAgentStore {
-    lateinit var agent: MeeAgent
+    private val agent: MeeAgent
     init {
 
+        var appDir = ComponentActivity().applicationInfo.dataDir + "/mee"
+        agent = getAgent(
+            MeeAgentConfig(
+                appDir,
+                null,
+                MeeAgentDidRegistryConfig.DidKey
+            )
+        )
+    }
+
+    fun getAllItems (): List<Context> {
+        val contextsCore = agent.listMaterializedContexts();
+        val contexts = contextsCore.filterIsInstance<MaterializedContext.RelyingParty>()
+            .mapNotNull { rec ->
+                val currentProtocol = rec.data.protocol;
+                var claims: MutableList<ConsentRequestClaim> = mutableListOf()
+                if (currentProtocol is ContextProtocol.Siop) {
+                    val claimsArrayLast = currentProtocol.value.claims.size - 1
+                    if (claimsArrayLast > 0) {
+                        var coreClaims = currentProtocol.value.claims[claimsArrayLast]
+
+                        for (coreClaim in coreClaims) {
+                            val value = coreClaim.value
+                            val key = coreClaim.key
+                            if (value is OidcClaimParams) {
+                                val claim = ConsentRequestClaim(value, key)
+                                claims.add(claim)
+                            }
+
+                        }
+                    }
+
+                    val context = Context(
+                        currentProtocol.value.redirectUri,
+                        rec.did,
+                        claims,
+                        PartnerMetadata(currentProtocol.value.clientMetadata),
+                    )
+                    return@mapNotNull context
+                }
+
+            return@mapNotNull null
+        }
+        return contexts
     }
 }
