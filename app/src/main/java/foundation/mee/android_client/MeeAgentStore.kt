20fc4828b9ec76f10eb3da3b7a85f05fdd1e6e6a
VAmuzing <amuzik95@gmail.com>
2023-03-14 02:30:16 +0300

    Data storage changes

--- app/src/main/java/foundation/mee/android_client/MeeAgentStore.kt
+++ app/src/main/java/foundation/mee/android_client/MeeAgentStore.kt
@@ -1,10 +1,39 @@
 package foundation.mee.android_client
 
 import uniffi.mee_agent.MeeAgent
+import uniffi.mee_agent.MeeAgentConfig
+import uniffi.mee_agent.MeeAgentDidRegistryConfig
+import uniffi.mee_agent.getAgent
+import java.io.File
+import kotlin.system.exitProcess
 
 class MeeAgentStore {
-    lateinit var agent: MeeAgent
+    private lateinit var agent: MeeAgent
     init {
+        val folderURL = File("")
+        if (folderURL.exists() && folderURL.isDirectory) {
+            val dbURL = File(folderURL.absolutePath + "mee.sqlite")
+             try {
+                 agent = getAgent(config = MeeAgentConfig(dsUrl = dbURL.absolutePath, dsEncryptionPassword = null, didRegistryConfig = MeeAgentDidRegistryConfig.DidKey))
+            } catch (e: Exception) {
+                // Print something like in Swift
+                error("Failed to init MeeAgent")
+//                exitProcess(1)
+            }
+        }
+    }
 
+    fun getAllItems(): Array<Context>? {
+        try {
+            val contextsCore = agent.listMaterializedContexts()
+//            val contexts: Array<Context>? = contextsCore.reduce<Context>(
+//                return Context()
+//            )
+        }
+        catch (e: Exception) {
+            println("error getting all contexts: ${e.toString()}")
+            return null
+        }
+        return null
     }
 }
