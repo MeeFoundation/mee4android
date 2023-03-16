0eb1c10c9832703a8c421293e791cee64cbf5b63
VAmuzing <amuzik95@gmail.com>
2023-03-16 16:56:45 +0300

    Changes to style

--- app/src/main/java/foundation/mee/android_client/ui/theme/Type.kt
+++ app/src/main/java/foundation/mee/android_client/ui/theme/Type.kt
@@ -2,15 +2,32 @@ package foundation.mee.android_client.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import foundation.mee.android_client.R
+
+val publicSansFamily = FontFamily(
+    Font(R.font.publicsans_medium, FontWeight.Medium),
+    Font(R.font.publicsans_semibold, FontWeight.SemiBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = publicSansFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 17.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = publicSansFamily,
+        fontWeight = FontWeight.Medium,
         fontSize = 16.sp
     )
     /* Other default text styles to override
