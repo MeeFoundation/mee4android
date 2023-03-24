6f6cec3698b467586ad36531737aca7104f7cf72
VAmuzing <amuzik95@gmail.com>
2023-03-24 14:22:36 +0300

    Add FirstRunPage and resources for it

---
+++ app/src/main/java/foundation/mee/android_client/views/buttons/RejectButton.kt
@@ -0,0 +1,45 @@
+package foundation.mee.android_client.views.buttons
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import foundation.mee.android_client.ui.theme.MeeGreenPrimaryColor
+import foundation.mee.android_client.ui.theme.MeeIdentityAgentTheme
+import foundation.mee.android_client.ui.theme.publicSansFamily
+
+@Composable
+fun RejectButton(action: () -> Unit = {}, title: String) {
+    Button(
+        onClick = action,
+        border = BorderStroke(2.dp, MeeGreenPrimaryColor),
+        shape = AbsoluteRoundedCornerShape(10.dp),
+        modifier = Modifier
+            .sizeIn(maxHeight = 51.dp)
+            .fillMaxSize()
+    ) {
+        Text(
+            text = title,
+            color = MeeGreenPrimaryColor,
+            fontFamily = publicSansFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RejectButtonPreview() {
+    MeeIdentityAgentTheme() {
+        RejectButton(title = "Continue")
+    }
+}
