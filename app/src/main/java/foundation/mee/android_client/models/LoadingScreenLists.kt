86a8a28d4a1ac5a637d3fd623348cc73990c81a7
VAmuzing <amuzik95@gmail.com>
2023-04-21 06:00:57 +0300

    Add strings list for cycling

---
+++ app/src/main/java/foundation/mee/android_client/models/LoadingScreenLists.kt
@@ -0,0 +1,11 @@
+package foundation.mee.android_client.models
+
+import foundation.mee.android_client.R
+
+val loadingScreenTexts: List<Int?> = listOf(
+    R.string.loading_white_page_1,
+    R.string.loading_white_page_2,
+    R.string.loading_white_page_3,
+    R.string.loading_white_page_4,
+    null
+)
