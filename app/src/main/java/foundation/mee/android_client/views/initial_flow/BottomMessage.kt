a4798a3c6408be4e66a6046a1fdfdb534e636a57
Anthony Ivanov <aivanov@swiftinvention.com>
2023-06-15 17:34:08 +0200

    initial flow button padding fix

--- app/src/main/java/foundation/mee/android_client/views/initial_flow/BottomMessage.kt
+++ app/src/main/java/foundation/mee/android_client/views/initial_flow/BottomMessage.kt
@@ -4,8 +4,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -20,12 +25,19 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.material.Surface
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
+import foundation.mee.android_client.R
 import foundation.mee.android_client.ui.theme.DurationPopupBackground
+import foundation.mee.android_client.ui.theme.MeeIdentityAgentTheme
 import foundation.mee.android_client.ui.theme.SystemBlueLight
 import foundation.mee.android_client.ui.theme.publicSansFamily
+import foundation.mee.android_client.views.MeeWhiteScreen
+import foundation.mee.android_client.views.animations.ConsentPageAnimation
 
 @Composable
 fun BottomMessage(icon: Painter, iconSize: Dp, title: String, message: String, onNext: () -> Unit) {
@@ -69,6 +81,7 @@ fun BottomMessage(icon: Painter, iconSize: Dp, title: String, message: String, o
                 colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
                 modifier = Modifier
                     .padding(0.dp)
+                    .height(60.dp)
                     .fillMaxWidth()
             ) {
                 Surface(color = Color.White) {
@@ -78,7 +91,6 @@ fun BottomMessage(icon: Painter, iconSize: Dp, title: String, message: String, o
                         color = SystemBlueLight,
                         fontSize = 20.sp,
                         textAlign = TextAlign.Center,
-                        modifier = Modifier.padding(vertical = 18.dp)
 
                     )
                 }
@@ -86,5 +98,29 @@ fun BottomMessage(icon: Painter, iconSize: Dp, title: String, message: String, o
             }
         }
 
+    }
+}
+
+@Preview(showBackground = true, widthDp = 375, heightDp = 812)
+@Composable
+fun BottomMessagePreview() {
+    MeeIdentityAgentTheme {
+        Box {
+            MeeWhiteScreen(modifier = Modifier.zIndex(2f), isFaded = true)
+            Column(
+                verticalArrangement = Arrangement.Bottom,
+                modifier = Modifier
+                    .zIndex(1f)
+                    .fillMaxHeight(),
+            ) {
+                BottomMessage(
+                    icon = painterResource(R.drawable.mee_guy_icon),
+                    iconSize = 60.dp,
+                    title = "Set up your Privacy Agent",
+                    message = "To store your data, Mee contains a secure data vault, which is protected by Biometry. Please set up Biometry.",
+                    onNext = {}
+                )
+            }
+        }
     }
 }
